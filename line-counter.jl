ee6b4be2037141b707a3b75fd56827c8183128bc
CodyKoInABox <codykoinabox@gmail.com>
2025-02-24 22:23:29 -0300

    can detect programming langague too

--- line-counter.jl
+++ line-counter.jl
@@ -1,7 +1,5 @@
 
-
-
-function count_lines(filename)
+function count_total_lines(filename)
     count = 0
     for _ in eachline(filename)
         count += 1
@@ -9,7 +7,30 @@ function count_lines(filename)
     return count
 end
 
-# bota o nome do arquivo aqui | file name goes here
-lines = count_lines("code-to-analyze/matrix.c")
+function get_file_type(filename)
 
+    # pegar a exntesao do arquivo
+    ext = splitext(filename)[2]
+
+    # jump table pq nao tem switch em julia (o que e muito bom ninguem mais usa switch case em 2024 por isso julia e tao legal vamos usar julia)
+    table = Dict(
+        ".c" => "C",
+        ".js" => "JavaScript",
+        ".java" => "Java",
+        ".jl" => "Julia",
+        ".py" => "Python"
+    )
+
+    return get(table, ext, "Other")
+end
+
+
+
+# bota o nome do arquivo aqui | file name goes here
+filename = "code-to-analyze/matrix.c"
+
+lines = count_total_lines(filename)
+fileType = get_file_type(filename)
+
+println("Program language: ", fileType)
 println("Total lines: ", lines)
